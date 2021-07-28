c9fc6efd457189b15f92a6e68e8cb8cf868eb511
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-28 15:25:18 +0200

    separate into test cases

--- tests/lcs.ts
+++ tests/lcs.ts
@@ -2,28 +2,41 @@ import { expect } from 'chai';
 import { LongestCommonSubsequenceSolver } from '../src/lcs';
 
 describe('longestCommonSubsequence', () => {
-  it('should find longest common subsequence for two strings', () => {
+  
+  it('should find longest common subsequence for two strings #1', () => {
     expect((new LongestCommonSubsequenceSolver([''], [''])).longestCommonSubsequence()).to.deep.equal(['']);
-
+  });
+  
+  it('should find longest common subsequence for two strings #2', () => {
     expect((new LongestCommonSubsequenceSolver([''], ['A', 'B', 'C'])).longestCommonSubsequence()).to.deep.equal(['']);
-
+  });
+  
+  it('should find longest common subsequence for two strings #3', () => {
     expect((new LongestCommonSubsequenceSolver(['A', 'B', 'C'], [''])).longestCommonSubsequence()).to.deep.equal(['']);
-
+  });
+  
+  it('should find longest common subsequence for two strings #4', () => {
     expect((new LongestCommonSubsequenceSolver(
       ['A', 'B', 'C'],
       ['D', 'E', 'F', 'G'],
     )).longestCommonSubsequence()).to.deep.equal(['']);
-
+  });
+  
+  it('should find longest common subsequence for two strings #5', () => {
     expect((new LongestCommonSubsequenceSolver(
       ['A', 'B', 'C', 'D', 'G', 'H'],
       ['A', 'E', 'D', 'F', 'H', 'R'],
     )).longestCommonSubsequence()).to.deep.equal(['A', 'D', 'H']);
-
+  });
+  
+  it('should find longest common subsequence for two strings #6', () => {
     expect((new LongestCommonSubsequenceSolver(
       ['A', 'G', 'G', 'T', 'A', 'B'],
       ['G', 'X', 'T', 'X', 'A', 'Y', 'B'],
     )).longestCommonSubsequence()).to.deep.equal(['G', 'T', 'A', 'B']);
-
+  });
+  
+  it('should find longest common subsequence for two strings #7', () => {
     expect((new LongestCommonSubsequenceSolver(
       ['A', 'B', 'C', 'D', 'A', 'F'],
       ['A', 'C', 'B', 'C', 'F'],
